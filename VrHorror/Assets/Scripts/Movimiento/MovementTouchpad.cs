c31b67373c0ed36c9373a741d85a80866c837bff
Ethese <f.goyeneche.camus@gmail.com>
2019-02-04 13:02:09 -0300

    Scripts varios

--- VrHorror/Assets/Scripts/Movimiento/MovementTouchpad.cs
+++ VrHorror/Assets/Scripts/Movimiento/MovementTouchpad.cs
@@ -5,7 +5,7 @@ using Valve.VR;
 
 public class MovementTouchpad : MonoBehaviour
 {
-    //[SteamVR_DefaultAction("MoveTouchpad")]
+    [SteamVR_DefaultAction("MoveTouchpad")]
     public float speed;
     private Vector2 movimiento = Vector2.zero;
     public Transform cameraRig;
@@ -22,10 +22,7 @@ public class MovementTouchpad : MonoBehaviour
 
     void Movement()
     {
-        //cameraRig.transform.eulerAngles = new Vector3(0, rotation, 0);
-        //movimiento = SteamVR_Input._default.inActions.MoveTouchpad.GetAxis(SteamVR_Input_Sources.Any);
-        //cameraRig.transform.position = transform.position + transform.forward * speed * Time.deltaTime;
-        //rb.MovePosition(transform.position + (transform.right * movimiento.x * speed) + (transform.forward * movimiento.y * speed));
+        movimiento = SteamVR_Input._default.inActions.MoveTouchpad.GetAxis(SteamVR_Input_Sources.Any);
         cameraRig.position += (transform.right * movimiento.x + transform.forward * movimiento.y) * Time.deltaTime * speed;
         cameraRig.position = new Vector3(cameraRig.position.x, cameraRig.position.y, cameraRig.position.z);
     }
