a4617e584b86f9dc43d643aedd2feb2afc32eb6b
Cheche <f.goyeneche.camus@gmail.com>
2019-02-05 13:50:57 -0300

    Creacion de checkpoints

---
+++ VrHorror/Assets/Scripts/Checkpoint/Checkpoint.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using vida;
+
+public class Checkpoint : MonoBehaviour
+{
+    public VidaJugador vidaPlayer;
+
+    Renderer rnd;
+    public Material checkpointOff;
+    public Material checkpointOn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        vidaPlayer = FindObjectOfType<VidaJugador>();
+        rnd = GetComponent<Renderer>();
+    }
+
+    void CheckpointOn()
+    {
+        Checkpoint[] checkpoints = FindObjectsOfType<Checkpoint>();
+        foreach(Checkpoint cp in checkpoints)
+        {
+            cp.CheckpointOff();
+        }
+
+        rnd.material = checkpointOn;
+    }
+
+    void CheckpointOff()
+    {
+        rnd.material = checkpointOff;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag.Equals("Player"))
+        {
+            vidaPlayer.SetRespawnPoint(transform.position);
+            CheckpointOn();
+        }
+    }
+}
--- VrHorror/Assets/Scripts/Vida/VidaJugador.cs
+++ VrHorror/Assets/Scripts/Vida/VidaJugador.cs
@@ -13,18 +13,19 @@ namespace vida
         public int startingLifes = 5;
         public int currentLifes;
         bool isDead;                                                // Whether the player is dead.
-        public Text uiVida;
-        public Text uiVidas;
+       // public Text uiVida;
+       // public Text uiVidas;
         float timer;
         float timeToRespawn = 15f;
+        public Vector3 respawn;
 
         void Awake()
         {
             // Set the initial health of the player.
             currentHealth = startingHealth;
             currentLifes = startingLifes;
-            uiVida.text = ("Vida: " + currentHealth);
-            uiVidas.text = ("Vidas: " + currentLifes);
+           // uiVida.text = ("Vida: " + currentHealth);
+           // uiVidas.text = ("Vidas: " + currentLifes);
 
         }
 
@@ -43,7 +44,7 @@ namespace vida
 
             // Reduce the current health by the damage amount.
             currentHealth -= amount;
-            uiVida.text = ("Vida: " + currentHealth);
+           // uiVida.text = ("Vida: " + currentHealth);
 
             // If the player has lost all it's health and the death flag hasn't been set yet...
             if (currentHealth <= 0 && !isDead)
@@ -56,7 +57,7 @@ namespace vida
         public void TakeHeal(int amount)
         {
             currentHealth += amount;
-            uiVida.text = ("Vida: " + currentHealth);
+           // uiVida.text = ("Vida: " + currentHealth);
 
             // If the player has lost all it's health and the death flag hasn't been set yet...
             if (currentHealth <= 0 && !isDead)
@@ -70,7 +71,7 @@ namespace vida
         {
             // Set the death flag so this function won't be called again.
             anim.SetTrigger("FadeOut");
-            uiVida.text = ("Vida: " + "0");
+          //  uiVida.text = ("Vida: " + "0");
             isDead = true;
             currentLifes = currentLifes - 1;
         }
@@ -79,11 +80,13 @@ namespace vida
         {
             if (isDead && currentLifes >= 0 )
             {
-                uiVidas.text = ("Vidas: " + currentLifes);
+              //  uiVidas.text = ("Vidas: " + currentLifes);
                 currentHealth = 100;
                 Debug.Log("Moriste" + currentLifes);
+                gameObject.GetComponent<Transform>().position = respawn;
                 anim.SetTrigger("FadeIn");
-                uiVida.text = ("Vida: " + currentHealth);
+              //  uiVida.text = ("Vida: " + currentHealth);
+                
                 isDead = false;
             }
         }
@@ -94,6 +97,11 @@ namespace vida
             {
                 yield return null;
             } while (animation.isPlaying);
+        }
+
+        public void SetRespawnPoint(Vector3 newPosition)
+        {
+            respawn = newPosition;
         }
     }
 }
