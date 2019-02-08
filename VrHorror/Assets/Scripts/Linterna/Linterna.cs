f6d2c84acac3e26630dc6ace2c50825995072f95
Puppiesonac1d <the_sorr0w@hotmail.com>
2019-02-08 13:18:59 -0300

    Linterna

---
+++ VrHorror/Assets/Scripts/Linterna/Linterna.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Linterna : MonoBehaviour
+{
+    public GameObject linterna;
+   
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("14"))
+        {
+            encender();
+        }
+    }
+
+    void encender()
+    {
+        linterna.SetActive(true);
+    }
+
+    void apagar()
+    {
+        linterna.SetActive(false);
+    }
+}
