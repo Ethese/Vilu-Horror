739145ec983728ba1156cadf6791c3ae83631bbb
jorgellanos <jorg.llanos@hotmail.com>
2019-02-27 20:12:15 -0300

    TutorialBETA1.2
    more woods

---
+++ VrHorror/Assets/Scripts/LevelEvents/TutorialManager.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialManager : MonoBehaviour
+{
+    public GameObject obj1, obj2;
+    public TextMesh txt1, txt2, txt3;
+
+    public bool ev1, ev2, ev3, st1Ready;
+    public bool ev4, ev5, ev6, st2ready;
+
+    private Interact i1, i2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        i1 = obj1.GetComponent<Interact>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Event1();
+
+        if (ev1)
+        {
+            Event2();
+        }
+
+        if (ev2)
+        {
+            Event3();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "stage2")
+        {
+            st1Ready = true;
+        }
+    }
+
+    public void Event1()// pick up
+    {
+        if (i1.examined)
+        {
+            ev1 = true;
+        }
+    }
+
+    public void Event2()// Throw
+    {
+        
+    }
+
+    public void Event3()// Move
+    {
+        
+    }
+}
---
+++ VrHorror/Assets/Scripts/LevelEvents/TutorialNPC.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialNPC : MonoBehaviour
+{
+    public TutorialManager tm;
+    public Transform pos1, pos2, pos3;
+
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Stage1();
+    }
+
+    public void ChangePosition(Vector3 posB)
+    {
+        speed = speed * Time.deltaTime;
+        transform.localPosition = Vector3.MoveTowards(transform.localPosition, posB, speed);
+    }
+
+    public void Stage1()
+    {
+        if (tm.ev2) // al terminar el segundo evento se prepara para el tercero
+        {
+            ChangePosition(pos1.position);
+        }
+    }
+}
