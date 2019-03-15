c0ef4792136e827c0ddf0e4c6c39c47e8daea4b6
Ethese <f.goyeneche.camus@gmail.com>
2019-03-15 18:07:03 -0300

    Rail Movement

---
+++ VrHorror/Assets/Scripts/RailCar/MovementRail.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementRail : MonoBehaviour
+{
+    public Rail rail;
+
+    private int currentSeg;
+    private float transition;
+    private bool isCompleted;
+
+    private void Update()
+    {
+        if (!rail)
+        {
+            return;
+        }
+        if (!isCompleted)
+        {
+            Play();
+        }
+    }
+
+    private void Play()
+    {
+        transition += Time.deltaTime * 1 / 2.5f;
+        if (transition > 1)
+        {
+            transition = 0;
+            currentSeg++;
+        }else if(transition < 0)
+        {
+            transition = 1;
+            currentSeg++;
+        }
+
+        transform.position = rail.linealPosition(currentSeg, transition);
+        transform.rotation = rail.Orientaion(currentSeg, transition);
+    }
+}
