83397cf7bc776708f4fd1e50976307f575a4b9f8
jorgellanos <jorg.llanos@hotmail.com>
2019-03-20 21:54:34 -0300

    The door 1.5
    se viene

--- VrHorror/Assets/Scripts/Interact/Door.cs
+++ VrHorror/Assets/Scripts/Interact/Door.cs
@@ -5,31 +5,34 @@ using Valve.VR;
 
 public class Door : MonoBehaviour
 {
-    public GameObject door;
+    public GameObject hand;
     public Hand h;
+
+    public float rotSpeed;
+    public bool grabbed;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        h = hand.GetComponent<Hand>();
+        rotSpeed = 5f;
+        grabbed = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (grabbed)
+        {
+            DoorRotation();
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Hand")
         {
-            Debug.Log("touching");
-
-            if (h.pressing || Input.GetKey("space"))
-            {
-                door.transform.LookAt(other.transform);
-            }
+            grabbed = true;
         }
     }
 
@@ -37,17 +40,30 @@ public class Door : MonoBehaviour
     {
         if (other.tag == "Hand")
         {
-
-            if (h.pressing)
-            {
-                door.transform.LookAt(other.transform);
-            }
+            grabbed = true;
         }
     }
 
     private void OnTriggerExit(Collider other)
     {
-        
+        if (other.tag == "Hand")
+        {
+            grabbed = false;
+        }
+    }
+
+    public void DoorRotation()
+    {
+        if (h.pressing)
+        {
+            /*Vector3 dir = hand.transform.position - transform.position;
+            Quaternion rotation = Quaternion.LookRotation(dir);
+            transform.rotation = Quaternion.Lerp(transform.rotation, rotation, rotSpeed);
+            */
+
+            
+
+        }
     }
 
 }
