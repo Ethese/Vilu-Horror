d52920d80804343e4e0667a3e30e12d42a601289
jorgellanos <jorg.llanos@hotmail.com>
2019-03-22 23:09:43 -0300

    The Door1.7 falta poco
    Creo

--- VrHorror/Assets/Scripts/Interact/Door.cs
+++ VrHorror/Assets/Scripts/Interact/Door.cs
@@ -5,23 +5,25 @@ using Valve.VR;
 
 public class Door : MonoBehaviour
 {
-    public GameObject hand;
+    public GameObject hand, limitA, limitB;
     public Hand h;
 
-    public float rotSpeed;
-    public bool grabbed;
+    public float rotSpeed, rotMax, rotMin, rotationY;
+    public float y;
+    public bool grabbed, limit;
     
     // Start is called before the first frame update
     void Start()
     {
         h = hand.GetComponent<Hand>();
         grabbed = false;
+        y = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (grabbed)
+        if (grabbed && !limit)
         {
             DoorRotation();
         }
@@ -33,13 +35,26 @@ public class Door : MonoBehaviour
         {
             grabbed = true;
         }
+
+        if (other.gameObject.name == "LimitA")
+        {
+            Debug.Log("STOOOOP");
+            limit = true;
+            y = -1;
+        }
+
+        if (other.gameObject.name == "LimitB")
+        {
+            limit = true;
+            y = 1;
+        }
     }
 
     private void OnTriggerStay(Collider other)
     {
         if (other.tag == "Hand")
         {
-            grabbed = true;
+            //grabbed = true;
         }
     }
 
@@ -49,7 +64,14 @@ public class Door : MonoBehaviour
         {
             grabbed = false;
         }
+
+        if (other.gameObject.name == "LimitB")
+        {
+            limit = false;
+            y = 0;
+        }
     }
+
 
     public void DoorRotation()
     {
@@ -59,6 +81,14 @@ public class Door : MonoBehaviour
             Vector3 dir = new Vector3((hand.transform.position.x - transform.position.x), 0, (hand.transform.position.z - transform.position.z));
             Quaternion rotation = Quaternion.LookRotation(dir);
             transform.rotation = Quaternion.Lerp(transform.rotation, rotation, rotSpeed);
+        }
+    }
+
+    public void Clamp()
+    {
+        if (limit)
+        {
+            transform.Rotate(Vector3.up, (y * rotSpeed) * Time.deltaTime);
         }
     }
 
