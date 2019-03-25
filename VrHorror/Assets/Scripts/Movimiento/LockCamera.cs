f6052e4ebbd2abaa70e98eb452e39b83c579baa2
Ethese <f.goyeneche.camus@gmail.com>
2019-03-25 17:19:30 -0300

    25.03.2019

---
+++ VrHorror/Assets/Scripts/Movimiento/LockCamera.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LockCamera : MonoBehaviour
+{
+    public Transform asiento;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = asiento.position;
+        Debug.Log("Posision actual: " + transform.position);
+    }
+}
---
+++ VrHorror/Assets/Scripts/RailCar/CarMovement3.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class CarMovement3 : MonoBehaviour
+{
+    public NavMeshAgent agent;
+    //Trsnforms
+    public Transform[] puntos;
+    public Transform puntoFinal;
+    private Transform puntoInicio;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        puntoInicio = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void Recorrido()
+    {
+        agent.SetDestination(new Vector3(puntoFinal.position.x, puntoFinal.position.y, puntoFinal.position.z));
+    }
+}
