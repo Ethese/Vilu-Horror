91c338047a28188b303b603b675bd50fac5b76e3
Ethese <f.goyeneche.camus@gmail.com>
2019-03-25 18:59:18 -0300

    Terreno Pintado

---
+++ VrHorror/Assets/Scripts/Inventario/Inventario.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+//using UnityStandardAssets.Characters.FirstPerson;
+using System;
+using UnityEngine;
+
+public class Inventario : MonoBehaviour
+{
+
+    public static bool Pausado = false;
+    public GameObject Menu;
+
+    private const int slots = 9;
+
+    private List<InventarioItem> mItems = new List<InventarioItem>();
+
+    public event EventHandler<InventarioEventArgs> ItemAnadido;
+
+    public void AnadirItem(InventarioItem item)
+    {
+        if (mItems.Count < slots) 
+        {
+            Collider collider = (item as MonoBehaviour).GetComponent<Collider>();
+            if (collider.enabled)
+            {
+                collider.enabled = false;
+                mItems.Add(item);
+                item.Recogido();
+
+                if (ItemAnadido !=null)
+                {
+                    ItemAnadido(this,new InventarioEventArgs(item));
+                }
+            }
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            if (Pausado)
+            {
+                VolverAlJuego();
+            }
+            else
+            {
+
+                Pausa();
+            }
+        }
+    }
+
+    public void VolverAlJuego()
+    {
+        Menu.SetActive(false);
+        Time.timeScale = 1f;
+        //GameObject.Find("FPSController").GetComponent<FirstPersonController>().enabled = true;
+        Pausado = false;
+    }
+
+    public void Pausa()
+    {
+        Menu.SetActive(true);
+        Time.timeScale = 0f;
+        //GameObject.Find("FPSController").GetComponent<FirstPersonController>().enabled = false;
+        Pausado = true;
+
+    }
+
+}
---
+++ VrHorror/Assets/Scripts/RailCar/Rail.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class Rail : MonoBehaviour
+{
+    [ExecuteInEditMode]
+    private Transform[] nodes;
+
+    private void Start()
+    {
+        nodes = GetComponentsInChildren<Transform>();
+    }
+
+    public Vector3 linealPosition(int seg, float ratio)
+    {
+        Vector3 p1 = nodes[seg].position;
+        Vector3 p2 = nodes[seg + 1].position;
+
+        return Vector3.Lerp(p1, p2, ratio);
+    }
+
+    public Quaternion Orientaion(int seg, float ratio)
+    {
+        Quaternion q1 = nodes[seg].rotation;
+        Quaternion q2 = nodes[seg + 1].rotation;
+
+        return Quaternion.Lerp(q1, q2, ratio);
+    }
+
+    private void OnDrawGizmos()
+    {
+        for (int i = 0; i < nodes.Length - 1; i++)
+        {
+            Handles.DrawDottedLine(nodes[i].position, nodes[i+1].position, 3.0f);
+
+        }
+    }
+}
