429f5e2b4bb97d165bfa2d244528b6f3d17f85c7
Ethese <f.goyeneche.camus@gmail.com>
2019-04-09 12:56:50 -0400

    Mountain Impostor added

---
+++ VrHorror/Assets/_Scripts/RailCar/StopDriving.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Vehicles.Car;
+
+public class StopDriving : MonoBehaviour
+{
+    public CarAIControl carAi;
+    public GameObject RigDriving;
+    public GameObject Rigstanding;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "car")
+        {
+            carAi.m_Driving = false;
+            Debug.Log("choco");
+            RigDriving.SetActive(false);
+            Rigstanding.SetActive(true);
+        }
+    }
+}
