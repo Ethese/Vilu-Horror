44c886b70698db10ed5b003a651c89ecbfdba817
JorgeLlanos <jorg.llanos@hotmail.com>
2019-04-12 00:26:57 -0400

    Gonna give it to YOU

---
+++ VrHorror/Assets/Prefabs/WHAT/ENEMY.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ENEMY : MonoBehaviour
+{
+    public bool chasing;
+    public float speed;
+
+    public AudioSource ad;
+    public Transform pl;
+    public Animator an;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        an = GetComponent<Animator>();
+        ad = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (chasing || Input.GetKey("space"))
+        {
+            ad.Play();
+            an.SetBool("RUN", true);
+            Chase(pl.position);
+        }
+    }
+
+    public void Chase(Vector3 target)
+    {
+
+        float moving = speed * Time.deltaTime;
+        transform.localPosition = Vector3.MoveTowards(new Vector3(transform.localPosition.x, transform.localPosition.y, transform.localPosition.z), target, moving);
+    }
+}
--- VrHorror/Assets/_Scripts/LevelEvents/Events.cs
+++ VrHorror/Assets/_Scripts/LevelEvents/Events.cs
@@ -6,34 +6,71 @@ public class Events : MonoBehaviour
 {
     public Checkpoint cp,cp1,cp2,cp3;
 
-    public GameObject cinematicPl, player;
+    public GameObject cinematicPl, player, light;
 
-    public AudioSource car1, car2;
+    public AudioSource car1, car2, rain;
+
+    public float time;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        time = 2;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (cp.check)
+        if (cinematicPl.activeSelf)
         {
-            CarScene();
+            car1.Stop();
+            Timer();
+            if (time == 0)
+            {
+                player.SetActive(true);
+                cinematicPl.SetActive(false);
+            }
+        }
+
+        if (cp.check || Input.GetKey("1"))
+        {
+            Phase1();
+        }
+
+        if (cp1.check || Input.GetKey("2"))
+        {
+            Phase2();
+        }
+
+        if (cp2.check || Input.GetKeyDown("3"))
+        {
+            Phase3();
+        }
+
+    }
+
+    public void Timer()
+    {
+        time -= 1 * Time.deltaTime;
+
+        if (time <= 0)
+        {
+            time = 0;
         }
     }
 
-    public void CarScene()
+    public void Phase1()
     {
-        car1.Stop();
-        if (cp1.check)
-        {
-            car2.Play();
-            cinematicPl.SetActive(false);
-            player.SetActive(true);
-        }
+        car2.Play();
     }
-    
+
+    public void Phase2()
+    {
+        rain.Stop();
+    }
+
+    public void Phase3()
+    {
+        light.transform.Rotate(-90,0,0);
+    }
 }
